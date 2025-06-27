b9cbc2c576e25a30e87156375a44722918550472
tembob64 <tbobokhidze@gmail.com>
2025-06-27 15:29:56 +0400

    Update 27.06.25

--- Include/DirectMemoryReader.h
+++ Include/DirectMemoryReader.h
@@ -34,13 +34,13 @@ private:
   const unsigned long long  mPenaltyType1Offset = 0x4069F8CuLL; //Byte mPenaltyType1 0x  156B478uLL
   const unsigned long long  mCutsOffset = 0x406A188uLL; //  2 mCutsOffset 0x156B678uLL
 
-  const unsigned long long mMotorMapOffset = 0x19F7628uLL; //41DBEE8 offset +0x48
-  const unsigned long long mChangedParamTypeOffset = 0x19F75D0uLL; //0x19ED430uLL; //0x405FCB0uLL; offset -0x10
-  const unsigned long long mChangedParamValueOffset = 0x19F75E0uLL; //0x19ED440uLL; //0x19ED440uLL; offset 0x00
+  const unsigned long long mMotorMapOffset = 0x19F80E8uLL; //41DBEE8 offset +0x48
+  const unsigned long long mChangedParamTypeOffset = 0x19F8090uLL; //0x19F75D0uLL; //0x19ED430uLL; //0x405FCB0uLL; offset -0x10
+  const unsigned long long mChangedParamValueOffset = 0x19F80A0uLL; //0x19F75E0uLL; //0x19ED440uLL; //0x19ED440uLL; offset 0x00
   const unsigned long long mChangeParamSlotStep = 0x1EEC0uLL; //0x11DB8uLL;
   const unsigned long long mChangeParamSlotStepInt = 0x7BB0uLL;
-  const unsigned long long mFront_ABR_Offset = 0x19FC9F4uLL; //0x19F2854uLL;
-  const unsigned long long mRear_ABR_Offset = 0x19FCA84uLL; //0x19F28E4uLL;
+  const unsigned long long mFront_ABR_Offset = 0x19FD4B4uLL; //0x19FC9F4uLL; //0x19F2854uLL;
+  const unsigned long long mRear_ABR_Offset = 0x19FD544uLL; //0x19FCA84uLL; //0x19F28E4uLL;
   
   
  
